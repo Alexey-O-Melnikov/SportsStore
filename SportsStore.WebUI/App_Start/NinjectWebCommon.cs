33853554247ff866e3826a75b8ee0688e8a047ad
As48rus <as48ru@yandex.ru>
2016-03-18 15:53:16 +0300

    I set up links and project structure

--- SportsStore.WebUI/App_Start/NinjectWebCommon.cs
+++ SportsStore.WebUI/App_Start/NinjectWebCommon.cs
@@ -11,6 +11,8 @@ namespace SportsStore.WebUI.App_Start
     using Ninject;
     using Ninject.Web.Common;
 
+    using System.Web.Mvc;
+
     public static class NinjectWebCommon 
     {
         private static readonly Bootstrapper bootstrapper = new Bootstrapper();
@@ -53,6 +55,7 @@ namespace SportsStore.WebUI.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
+            DependencyResolver.SetResolver(new SportsStore.WebUI.Infrastructure.NinjectDependencyResolver(kernel));
         }        
     }
 }
